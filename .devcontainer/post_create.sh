c0d5da7ed28128e15244145a0d5a435c4ceef165
atsuhiro.hada <atsuhiro.hada@brainpad.co.jp>
2024-10-30 17:59:12 +0000

    [add] devcontainer開発環境を整備

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get install -y wget bash git gcc g++ gfortran  liblapack-dev libamd2 libcholmod3 libmetis-dev libsuitesparse-dev libnauty2-dev
+cd ~/
+wget -nH https://raw.githubusercontent.com/coin-or/coinbrew/master/coinbrew
+chmod u+x coinbrew
+bash coinbrew fetch Cbc@master
+bash coinbrew build Cbc@master --no-prompt --prefix=/usr/local --tests=none --enable-cbc-parallel
